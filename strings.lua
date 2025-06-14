f5a18335a5bc3adef567705a874e430962ed325e
Florian Thiel <flth@flth.de>
2025-06-14 16:51:05 +0200

    Anpassung strings

--- strings.lua
+++ strings.lua
@@ -4,8 +4,17 @@ return {
 
 		["mod_name"] = "MB O 303",
 		["mod_desc"] = "The MB O 303 is considered one of the world's most successful coaches. The series was introduced as the successor to the O 302 in 1974 and was built until 1992. In the beginning, the windscreen was still fitted with a centre bar, but from 1979 a one-piece windscreen was also offered as an option. The biggest facelift took place in 1982, when mainly the front was changed. Most of the technology was known from the predecessor and was not changed during the entire production period. There was a wide range of model series and variations, from the short 9.2 m bus to the 12 m bus, from the regular-service bus to the luxurious high-deck long-distance coach. In 1992, production ended after more than 38,000 units and the successor O 404 was introduced.",
-		
+
 		["MB_0303_menu"] = "MB O 303",
+
+        ["MB_O303_1_alt_name"] = "MB O 303 13 RHS Club coach",
+        ["MB_O303_2_alt_name"] = "MB O 303 13 RHS Club coach VHH",
+        ["MB_O303_3_alt_name"] = "MB O 303 15 RHS Long-distance coach \"Globus Reisen\"",
+        ["MB_O303_4_alt_name"] = "MB O 303 15 RHS Long-distance coach \"Reisedienst Gründau\"",
+        ["MB_O303_7_alt_name"] = "MB O 303 15 RHS Long-distance coach \"Barbi's'\"",
+        ["MB_O303_8_alt_name"] = "MB O 303 15 KHP Excursion bus Bahnbus-Reisedienst",
+        ["MB_O303_9_alt_name"] = "MB O 303 9 RHS Excursion bus BVG",
+        ["MB_O303_10_alt_name"] = "MB O 303 9 KHP Excursion bus \"Horst Beelitz\"",
 
         ["MB_O303_1_name"] = "MB O 303 13 RHS Club coach \"Vogel\" (fictional)",
         ["MB_O303_2_name"] = "MB O 303 13 RHS Club coach \"Pflüger\"",
@@ -17,16 +26,27 @@ return {
         ["MB_O303_8_name"] = "MB O 303 15 KHP Excursion bus \"Janssen\"",
         ["MB_O303_9_name"] = "MB O 303 9 RHS Excursion bus \"Dechant\"",
         ["MB_O303_10_name"] = "MB O 303 9 KHP Excursion bus \"Markert\"",
-        ["MB_O303_desc"] = "The MB O 303 was one of the most successful coaches. It was the successor of the O 302 and adopted much of its technology. The first years of construction were only delivered with a split windscreen. The chrome-plated glasses around the headlights and radiator grille were also typical.",
+        ["MB_O303_11_name"] = "MB O 303 9 KHP Excursion bus \"Mecki-Reisen\"",
+
+        ["MB_O303_desc"] = "The MB O 303 was one of the most successful coaches. It was the successor of the O 302 and adopted much of its technology. The first years of construction were only delivered with a split windscreen. The chrome-plated goggles around the headlights and radiator grille were also typical, but were removed from 1982 after the facelift.",
 		["MB_O303_Facelift_desc"] = "The MB O 303 was one of the most successful coaches. It was the successor of the O 302 and adopted much of its technology. After the facelift in 1982, the centre bar in the windscreen and the chrome-plated glasses around the headlights and radiator grille were removed. The model was built until 1992.",
-		
+
 	},
 	de = {
 
 		["mod_name"] = "MB O 303",
 		["mod_desc"] = "Der MB O 303 gilt als einer der weltweit erfolgreichsten Reisebusse. Die Bauserie wurde als Nachfolger des O 302 im Jahr 1974 vorgestellt und wurde bis 1992 gebaut. Anfangs war die Scheibe noch mit einem Mittelsteg versehen, ab 1979 wurde auch optional eine einteilige Scheibe angeboten. Das größte Facelift erfolgte im Jahr 1982, bei dem vor allem die Front verändert wurde. Die meiste Technik war vom Vorgänger bekannt und wurde die gesamte Bauzeit über nicht geändert. Es gab eine große Bandbreite von Modellreihen und -variationen, vom kurzen 9,2 m-Bus bis zum 12 m-Bus, vom Linien-Omnibus bis zum luxuriösen Hochdecker-Fernreisebus. 1992 endete nach über 38.000 Stück die Fertigung und der Nachfolger O 404 wurde vorgestellt.",
-		
+
 		["MB_0303_menu"] = "MB O 303",
+
+        ["MB_O303_1_alt_name"] = "MB O 303 13 RHS Clubbus",
+        ["MB_O303_2_alt_name"] = "MB O 303 13 RHS Clubbus VHH",
+        ["MB_O303_3_alt_name"] = "MB O 303 15 RHS Fernreisebus \"Globus Reisen\"",
+        ["MB_O303_4_alt_name"] = "MB O 303 15 RHS Fernreisebus \"Reisedienst Gründau\"",
+        ["MB_O303_7_alt_name"] = "MB O 303 15 RHS Fernreisebus \"Barbi's'\"",
+        ["MB_O303_8_alt_name"] = "MB O 303 15 KHP Ausflugsbus Bahnbus-Reisedienst",
+        ["MB_O303_9_alt_name"] = "MB O 303 9 RHS Ausflugsbus BVG",
+        ["MB_O303_10_alt_name"] = "MB O 303 9 KHP Ausflugsbus \"Horst Beelitz\"",
 
         ["MB_O303_1_name"] = "MB O 303 13 RHS Clubbus \"Vogel\" (fiktiv)",
         ["MB_O303_2_name"] = "MB O 303 13 RHS Clubbus \"Pflüger\"",
@@ -38,7 +58,9 @@ return {
         ["MB_O303_8_name"] = "MB O 303 15 KHP Ausflugsbus \"Janssen\"",
         ["MB_O303_9_name"] = "MB O 303 9 RHS Ausflugsbus \"Dechant\"",
         ["MB_O303_10_name"] = "MB O 303 9 KHP Ausflugsbus \"Markert\"",
-        ["MB_O303_desc"] = "Der MB O 303 war einer der erfolgreichsten Reisebusse. Er war Nachfolger der O 302 und übernahm viel von dessen Technik. Die ersten Baujahre wurden nur mit geteilter Frontscheibe ausgeliefert. Typisch war zudem die verchromte Brille um Scheinwerfer und Kühlerblende.",
+        ["MB_O303_11_name"] = "MB O 303 9 KHP Ausflugsbus \"Mecki-Reisen\"",
+
+        ["MB_O303_desc"] = "Der MB O 303 war einer der erfolgreichsten Reisebusse. Er war Nachfolger der O 302 und übernahm viel von dessen Technik. Die ersten Baujahre wurden nur mit geteilter Frontscheibe ausgeliefert. Typisch war zudem die verchromte Brille um Scheinwerfer und Kühlerblende, ab 1982 nach dem Facelift entfiel diese.",
 		["MB_O303_Facelift_desc"] = "Der MB O 303 war einer der erfolgreichsten Reisebusse. Er war Nachfolger der O 302 und übernahm viel von dessen Technik. Nach dem Facelift im Jahre 1982 entfiel der Mittelsteg in der Frontscheibe und die verchromte Brille um Scheinwerfer und Kühlerblende. Das Modell wurde bis 1992 gebaut.",
 
 	},
